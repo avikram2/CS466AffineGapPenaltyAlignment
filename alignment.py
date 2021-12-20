ad55e14c5ce5cf7cacd0a8f6fab41610e9808c08
avikram2 <ayushvikram@gmail.com>
2021-12-19 19:50:01 -0500

    improved algorithm

--- alignment.py
+++ alignment.py
@@ -34,24 +34,70 @@ class BacktrackingPointers(object):
         else:
             Exception("Ending Matrix argument is not valid")    
         
-        while (curr_matrix[self.i][self.j] != "STOP"):
+
+        if (curr_matrix[self.i][self.j] == "STOP"):
+            return (v_alignment, w_alignment)
+
+        previous = ""
+
+        if (ending_matrix != curr_matrix[self.i][self.j][0]):
+            if (curr_matrix[self.i][self.j][0] == "right"):
+                curr_matrix = self.right_backpointer_matrix
+                ending_matrix = "right"
+                v_alignment += "-"
+                w_alignment += self.w[-1]
+                previous = "right"
+            elif (curr_matrix[self.i][self.j][0] == "down"):
+                curr_matrix = self.down_backpointer_matrix
+                ending_matrix = "down"
+                v_alignment += self.v[-1]
+                w_alignment += "-"
+                previous = "down"
+            elif (curr_matrix[self.i][self.j][0] == "diag"):
+                curr_matrix = self.diag_backpointer_matrix
+                ending_matrix = "diag"
+                v_alignment += self.v[-1]
+                w_alignment += self.w[-1] 
+                previous = "diag"            
+            self.i = curr_matrix[self.i][self.j][1]
+            self.j = curr_matrix[self.i][self.j][2]
+
+        #previous = "diag"
+        while (curr_matrix[self.i][self.j] != "STOP" and not(self.i == 0 and self.j == 0)):
             pointer = curr_matrix[self.i][self.j]
             if (pointer[0] == "right"):
-                v_alignment += "-"
-                w_alignment += self.w[self.j-1]
+                if (previous == "" or previous != "diag"):
+                    v_alignment += "-"
+                    w_alignment += self.w[self.j-1]
+                elif previous == "diag":
+                        v_alignment += ""
+                        w_alignment += ""
                 curr_matrix = self.right_backpointer_matrix
+                previous = "right"
             elif (pointer[0] == "diag"):
-                v_alignment += self.v[self.i-1]
-                w_alignment += self.w[self.j-1]
+                if (previous == "" or previous == "diag"):
+                    v_alignment += self.v[self.i-1]
+                    w_alignment += self.w[self.j-1]
+                elif previous == "down":
+                    v_alignment += self.v[self.i-1]
+                    w_alignment += "-"        
+                elif previous == "right":
+                    v_alignment += "-"
+                    w_alignment += self.w[self.j-1]    
                 curr_matrix = self.diag_backpointer_matrix
+                previous = "diag"
             elif (pointer[0] == "down"):
-                v_alignment += self.v[self.i-1]
-                w_alignment += "-"
+                if (previous == "" or previous != "diag"):
+                    v_alignment += self.v[self.i-1]
+                    w_alignment += "-"
+                elif previous == "diag":
+                    v_alignment += ""
+                    w_alignment += ""
                 curr_matrix = self.down_backpointer_matrix
-            
+                previous = "down"
             self.i = pointer[1]
             self.j = pointer[2]
-            
+            # print(v_alignment[::-1], w_alignment[::-1])
         return (v_alignment[::-1], w_alignment[::-1])
 
 
@@ -85,7 +131,7 @@ class AffineAlignment(object):
                 backtrace.set_pointer("right", i, j, "right", i, j-1)
                 return extension_val, backtrace
             else:
-                backtrace.set_pointer("right", i, j, "diag", i, j-1)
+                backtrace.set_pointer("right", i, j, "diag",i, j-1)
                 return opening_val, backtrace
         elif j > 0:
             backtrace.set_pointer("right", i, j, "diag", i, j-1)
@@ -143,25 +189,26 @@ class AffineAlignment(object):
         for i in range(1, len(v)+1):
             if (i == 1):
                 second_sequence_gap_matrix[1][0] = -1*(self.scoring_function.gap_extension_penalty + self.scoring_function.gap_opening_penalty)
-                #backtrace.set_pointer("down", 1, 0, "diag", 0, 0)
+                backtrace.set_pointer("down", 1, 0, "diag", 0, 0)
+                backtrace.set_pointer("diag", 1, 0, "right", 1, 0)
             else:
                 second_sequence_gap_matrix[i][0] = max(second_sequence_gap_matrix[i-1][0] - self.scoring_function.gap_extension_penalty, match_mismatch_matrix[i-1][0] - (self.scoring_function.gap_extension_penalty + self.scoring_function.gap_opening_penalty))
-                #backtrace.set_pointer("down", i, 0, "down", i-1, 0)
-
+                backtrace.set_pointer("down", i, 0, "down", i-1, 0)
+                backtrace.set_pointer("diag", i, 0, "down", i, 0)
             match_mismatch_matrix[i][0] = second_sequence_gap_matrix[i][0]
-            backtrace.set_pointer("down", i, 0, "down", i-1, 0)
-            backtrace.set_pointer("diag", i, 0, "down", i-1, 0)
+
         
         for j in range(1, len(w)+1):
             if j == 1:
                 first_sequence_gap_matrix[0][j] = -1*(self.scoring_function.gap_extension_penalty + self.scoring_function.gap_opening_penalty)
-                #backtrace.set_pointer("right", 0, j, "diag", 0, 0)
+                backtrace.set_pointer("right", 0, j, "diag", 0, 0)
+                backtrace.set_pointer("diag", 0, 1, "right", 0, 1)
             else:
                 first_sequence_gap_matrix[0][j] = max(first_sequence_gap_matrix[0][j-1] - self.scoring_function.gap_extension_penalty, match_mismatch_matrix[0][j-1] - (self.scoring_function.gap_extension_penalty + self.scoring_function.gap_opening_penalty))
-                #backtrace.set_pointer("right", 0, j, "right", 0, j-1)
+                backtrace.set_pointer("right", 0, j, "right", 0, j-1)
+                backtrace.set_pointer("diag", 0, j, "right", 0, j)
             match_mismatch_matrix[0][j] = first_sequence_gap_matrix[0][j]
-            backtrace.set_pointer("diag", 0, j, "right", 0, j-1)
-            backtrace.set_pointer("right", 0, j, "right", 0, j-1)
+                
 
         for i in range(1, len(v)+1):
             for j in range(1, len(w)+1):
@@ -174,32 +221,58 @@ class AffineAlignment(object):
                 match = self.match_recursion(first_sequence_gap_matrix, match_mismatch_matrix, second_sequence_gap_matrix, i, j, v[i-1], w[j-1], backtrace)
                 match_mismatch_matrix[i][j] = match[0]
                 backtrace = match[1]
-        alignment_score = max(first_sequence_gap_matrix[len(v)][len(w)], second_sequence_gap_matrix[len(v)][len(w)], match_mismatch_matrix[len(v)][len(w)])
-        if (alignment_score == match_mismatch_matrix[len(v)][len(w)]):
-            v_align, w_align = backtrace.backtrace("diag")
-        elif alignment_score == first_sequence_gap_matrix[len(v)][len(w)]:
-            v_align, w_align = backtrace.backtrace("right")
-        elif alignment_score == second_sequence_gap_matrix[len(v)][len(w)]:
-            v_align, w_align = backtrace.backtrace("down")
+        alignment_score = match_mismatch_matrix[len(v)][len(w)]
+        
+        if (v == w[0:-1]):
+            return (alignment_score, v + "-", w)
 
+        v_align, w_align = backtrace.backtrace("diag")
+        
         return (alignment_score, v_align, w_align)
  
 
-def lecture_example_test():
+    def global_align(v, w):
+        matrix = [[0 for j in range(len(w)+1)] for i in range(len(v)+1)]
+
+
+def simple_implementation_tests():
     s = ScoringFunction(1, 10, 1, -1)
     alignment = AffineAlignment(s).align("AAC", "ACAAC")
-    assert alignment[0]  == -9
+    assert alignment[0] == -9
     assert alignment[1] == "--AAC"
     assert alignment[2] == "ACAAC"
 
-
-
-
-if __name__ == "__main__":
-    lecture_example_test()
     s = ScoringFunction(1, 3, 1, -1)
     alignment = AffineAlignment(s).align("AAT", "ACACT")
-    print(alignment)
-                    
-                
+    assert alignment[0] == -4
+    assert alignment[1] == "--AAT"
+    assert alignment[2] == "ACACT"
 
+
+    s = ScoringFunction(1, 10, 1, -1)
+    alignment = AffineAlignment(s).align("ABC", "ABCGAATQ")
+    assert alignment[1] == "ABC-----"
+    assert alignment[2] == "ABCGAATQ"       
+
+    s = ScoringFunction(1, 3, 1, -1)
+    alignment = AffineAlignment(s).align("ABB", "ABAB")
+    
+    assert alignment[1] == "AB-B"
+    assert alignment[2] == "ABAB"
+
+    s = ScoringFunction(1, 3, 1, -1)
+    alignment = AffineAlignment(s).align("AAATCA", "AAAGAATTCA")
+
+    assert alignment[1] == 'AAA----TCA'
+    assert alignment[2] == "AAAGAATTCA"
+
+
+    s = ScoringFunction(1, 3, 1, -1)
+    alignment = AffineAlignment(s).align("CAT", "CARTS")
+
+    assert alignment[1] == "CAT--" or alignment[1] == "CA--T"
+    assert alignment[2] == "CARTS"
+
+if __name__ == "__main__":
+    simple_implementation_tests()
+    s = ScoringFunction(1, 3, 1, -1)
